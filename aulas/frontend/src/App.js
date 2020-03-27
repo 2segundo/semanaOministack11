a31e1f0a752628fa33a71eb7ee90e8e9ae3ac389
2segundo <josevitorsegundo@hotmail.com>
2020-03-26 22:15:51 -0300

    Preparação página Logon

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './Header';
 
 // JSX (JavaScript XML)
 function App() {
+   /**
+    * estado - toda vez que um componente precisar armazenar uma info 
+    * dentro dele criaremos um estado, pois alteramos essas infos,
+    * e ela reflete na interface
+    * 
+   */
+  const [counter, setCounter] = useState(0);
+  // Array [Valor, funçãoDeAtualização]
+
+  function increment(){
+    setCounter(counter+1);
+  }
+
   return (
-    <Header title = "Semana OminiStack"/>
+    //<Header title = "Semana OminiStack"/> -- Entretanto no Header o propos tem que ser props.title
+    //<Header> Semana OminiStack </Header>
+    <div>
+    <Header>Contador: {counter}</Header>
+    <button onClick = {increment}>Incrementar</button>
+    </div>
   );
 }
 
